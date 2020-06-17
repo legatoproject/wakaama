01ce63fdcf86eb0d4765fb9326d7e6df36846f09
Frederic DUR <fdur@sierrawireless.com>
2020-06-17 14:46:13 +0200

    Memory leak while processing crafted packet
    Merge the Wakaama #425 and add unitary test
    Fix another leak
    
    Relates: LE-14808
    Resolves: LE-15890
    Change-Id: I19bb39afb0481d36e9f12ac2a86cbaccfd474545
    Signed-off-by: Frederic DUR <fdur@sierrawireless.com>
    (cherry picked from commit b1f3a8beec3e0cbaddc28a51cdd24097272ac899)

--- core/er-coap-13/er-coap-13.c
+++ core/er-coap-13/er-coap-13.c
@@ -839,6 +839,7 @@ coap_parse_message(void *packet, uint8_t *data, uint16_t data_len)
         if (option_number & 1)
         {
           coap_error_message = "Unsupported critical option";
+          coap_free_header(coap_pkt);
           return BAD_OPTION_4_02;
         }
     }
